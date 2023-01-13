1de02458da4860f5d9dede3a5682171bfd63fa29
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2023-01-13 23:08:25 +0530

    impl jasypt encryption

---
+++ playground/src/main/java/com/microservices/demo/jasypt/TestJasypt.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+    public static void main(String[] args) {
+        StandardPBEStringEncryptor pbeStringEncryptor = new StandardPBEStringEncryptor();
+        pbeStringEncryptor.setPassword("qwerty@123");
+        pbeStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        pbeStringEncryptor.setIvGenerator(new RandomIvGenerator());
+        String result = pbeStringEncryptor.encrypt("springCloud_Pwd");
+        System.out.println(result);
+        System.out.println(pbeStringEncryptor.decrypt(result));
+    }
+}
