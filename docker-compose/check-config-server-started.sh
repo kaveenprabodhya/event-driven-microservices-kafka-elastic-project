b50379d844318256a8723202ecc27a53a002772f
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2023-01-20 23:25:02 +0530

    remove edited config

--- docker-compose/check-config-server-started.sh
+++ docker-compose/check-config-server-started.sh
@@ -15,10 +15,4 @@ while [[ ! $curlResult == "200" ]]; do
   curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
 done
 
-#ls
-#
-#cd org/springframework || exit
-#
-#ls
-
 #./cnb/lifecycle/launcher
