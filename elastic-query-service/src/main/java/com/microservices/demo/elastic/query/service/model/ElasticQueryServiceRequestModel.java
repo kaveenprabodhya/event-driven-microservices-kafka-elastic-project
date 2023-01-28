a7d38ca5401bb293b6733556b4b0ab4dd581dc6f
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2023-01-28 22:18:52 +0530

    add validation

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceRequestModel.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceRequestModel.java
@@ -1,5 +1,6 @@
 package com.microservices.demo.elastic.query.service.model;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ElasticQueryServiceRequestModel {
     private String id;
+    @NotEmpty
     private String text;
 }
