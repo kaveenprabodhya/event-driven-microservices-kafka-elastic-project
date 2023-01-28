2e41899144351dd9845c5759d10747108eeddc4d
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2023-01-28 19:04:14 +0530

    enable encription properties

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/ElasticQueryServiceApplication.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/ElasticQueryServiceApplication.java
@@ -1,11 +1,13 @@
 package com.microservices.demo.elastic.query.service;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.microservices.demo")
+@EnableEncryptableProperties
 public class ElasticQueryServiceApplication {
 
     public static void main(String[] args) {
