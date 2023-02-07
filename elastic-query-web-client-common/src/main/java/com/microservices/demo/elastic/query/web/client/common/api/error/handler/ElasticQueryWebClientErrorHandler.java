755a812f31fa16599e5aa3ce5e5709526ff7caea
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2023-02-07 06:11:27 +0530

    commented handle web exchange exception

--- elastic-query-web-client-common/src/main/java/com/microservices/demo/elastic/query/web/client/common/api/error/handler/ElasticQueryWebClientErrorHandler.java
+++ elastic-query-web-client-common/src/main/java/com/microservices/demo/elastic/query/web/client/common/api/error/handler/ElasticQueryWebClientErrorHandler.java
@@ -4,16 +4,21 @@ package com.microservices.demo.elastic.query.web.client.common.api.error.handler
 import com.microservices.demo.elastic.query.web.client.common.model.ElasticQueryWebClientRequestModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ElasticQueryWebClientErrorHandler {
@@ -64,5 +69,16 @@ public class ElasticQueryWebClientErrorHandler {
         model.addAttribute("error_description", errors);
         return "home";
     }
+
+//    @ExceptionHandler(WebExchangeBindException.class)
+//    public ResponseEntity<List<String>> handleException(WebExchangeBindException e) {
+//        LOG.error("Method argument validation exception!", e);
+//        var errors = e.getBindingResult()
+//                .getAllErrors()
+//                .stream()
+//                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                .collect(Collectors.toList());
+//        return ResponseEntity.badRequest().body(errors);
+//    }
 
 }
