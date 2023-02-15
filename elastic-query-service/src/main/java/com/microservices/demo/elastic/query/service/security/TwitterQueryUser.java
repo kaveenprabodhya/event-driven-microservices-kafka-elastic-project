c245d3e81514aa3e91ff21008932a8c818bd656f
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2023-02-16 02:09:47 +0530

    add TwitterQueryUser

---
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/security/TwitterQueryUser.java
@@ -0,0 +1,55 @@
+package com.microservices.demo.elastic.query.service.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Builder
+@Getter
+public class TwitterQueryUser implements UserDetails {
+    private String username;
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
