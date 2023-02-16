35a5f4e6dbcb64fc209e569a3c53b5a3f61f96f8
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2023-02-16 06:42:06 +0530

    add permissions

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/security/TwitterQueryUser.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/security/TwitterQueryUser.java
@@ -6,17 +6,21 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Map;
+
+import static com.microservices.demo.elastic.query.service.Constants.NA;
 
 @Builder
 @Getter
 public class TwitterQueryUser implements UserDetails {
     private String username;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    private Map<String, PermissionType> permissions;
 
     public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
         this.authorities = authorities;
     }
-
-    private Collection<? extends GrantedAuthority> authorities;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -25,7 +29,7 @@ public class TwitterQueryUser implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return NA;
     }
 
     @Override
@@ -35,21 +39,21 @@ public class TwitterQueryUser implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
